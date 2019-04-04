521c79f043ff50a8ded2d3bc326dacfca5afd4cc
Fahim Kabir <fkabir101@gmail.com>
2019-04-04 17:29:44 -0400

    fixed naming

--- client/src/components/header/headerMaine.js
+++ client/src/components/header/headerMaine.js
@@ -5,7 +5,7 @@ import "./style/header.css";
 function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="header">
-      <a className="navbar-brand" id="title" href="/">Uni-Fight</a>
+      <a className="navbar-brand" id="title" href="/">Uni-Jam</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
